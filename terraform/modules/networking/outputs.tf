a5832de8fc26cd7d654253a0ed1ce1a568cddece
ab1355 <144258346+ab1355@users.noreply.github.com>
2025-03-19 20:49:27 -0400

    Add networking module outputs

---
+++ terraform/modules/networking/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "internal_security_group_id" {
+  description = "ID of the internal communication security group"
+  value       = aws_security_group.allow_internal.id
+}
